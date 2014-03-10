b1eeffdefe2372e52f046802ad4c5f823081b261
weyewe <w.yunnal@gmail.com>
2014-03-10 17:11:00 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Belajargit::Application.config.secret_key_base = 'b3c9f3bd57fbef8486f54018da0f4d308451410a3ebc71ead7a898102426fbbc4918978722fc08d0e50ad691f4d15585261999ea7350631bc053b50b0b2a0760'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Belajargit::Application.config.session_store :cookie_store, key: '_belajargit_session'
